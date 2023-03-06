0fe47a8dbf71e1e997dce4a1830fb6e16dd6cb39
Lương Anh Vũ <anhvuiter@gmail.com>
2023-03-07 01:23:55 +0700

    new menu tructure updated (based on dragon tale game tutorial)

---
+++ src/UserInterface/GameMain.java
@@ -0,0 +1,13 @@
+package UserInterface;
+
+import javax.swing.*;
+public class GameMain {
+    public static void main(String[] args) {
+        JFrame window = new JFrame ("Grybotector");
+        window.setContentPane(new GamePanel());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.pack();
+        window.setVisible(true);
+    }
+}
