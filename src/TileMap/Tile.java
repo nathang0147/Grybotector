8b0e262109a85fa65f58509742b9947b4b8aabca
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2023-03-08 16:00:23 +0700

    create Tile Class

---
+++ src/TileMap/Tile.java
@@ -0,0 +1,24 @@
+package TileMap;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+    private BufferedImage img;
+    private int type;
+    // tile Types: 1 Block ( vẽ ) 0 normal ( Ko vẽ)
+    public   final  static  int BLOCK=1;
+    public final  static  int NORMAL=0;
+
+    public  Tile(BufferedImage img,int type){
+        this.img= img;
+        this.type=type;
+
+    }
+    public BufferedImage getImg(){
+        return  img;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
---
+++ src/TileMap/TileMap.java
@@ -0,0 +1,17 @@
+package TileMap;
+
+public class TileMap {
+    //position (x,y)
+    private  double x;
+    private  double y;
+
+
+
+
+
+
+
+    public TileMap(){
+
+    }
+}
