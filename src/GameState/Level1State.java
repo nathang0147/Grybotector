f48d3139b2cd213a884e7ec0219d797f866248e5
Lương Anh Vũ <anhvuiter@gmail.com>
2023-04-06 20:44:47 +0700

    Config the background and source path for the map

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -1,12 +1,14 @@
 package GameState;
 
 import TileMap.TileMap;
-import UserInterface.GamePanel;
-
 import java.awt.*;
+import TileMap.Background;
 
 public class Level1State extends GameState{
+
+
     private TileMap tileMap;
+    private Background bg;
     public Level1State(GameStateManager gsm) {
         this.gsm = gsm;
         init();
@@ -14,9 +16,10 @@ public class Level1State extends GameState{
 
     public void init() {
         tileMap = new TileMap(32);
-        tileMap.loadTiles("/TileSet/Tilesheet.png");
-        tileMap.loadMap("/Map/TileMap1.txt");
+        tileMap.loadTiles("/TilesandTileMap/Tilesheet.png");
+        tileMap.loadMap("/TilesandTileMap/Tiledmap.tmx");
 
+        bg = new Background("/assets/background.png", 0.1);
     }
 
 
@@ -27,8 +30,13 @@ public class Level1State extends GameState{
 
     public void draw(Graphics2D g) {
         //clear bg ( not real bg)
-        g.setColor(Color.BLUE);
-        g.fillRect(0,0, GamePanel.WIDTH,GamePanel.HEIGHT);
+//
+//        g.setColor(new Color(127, 53, 24));
+//        g.fillRect(0,0, GamePanel.WIDTH,GamePanel.HEIGHT);
+
+        //Draw Image
+        bg.draw(g);
+
         //Draw
         tileMap.draw(g);
     }
