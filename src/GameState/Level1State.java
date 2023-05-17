999e387f82bc319a7c89b0d342f6616267001caa
Thành Duy <thanhduy1706@gmail.com>
2023-05-17 11:18:04 +0700

    update map & background level 2

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -24,8 +24,8 @@ public class Level1State extends GameState{
     public void init() {
         tileMap = new TileMap(32);
         tileMap.loadTiles("/TileSet/Tilesheet.png");
-        tileMap.loadMap("/Map/Map_edited.txt");
-        tileMap.setPosition(0,0); 
+        tileMap.loadMap("/Map/Map_level1.txt");
+        tileMap.setPosition(0,0);
 
         bg=new Background("/assets/background.png",0.1);
 
