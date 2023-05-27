e5a438d12c66d4d30e6509d848ba9b3e08357dd6
NguyenDucTam <116332835+NguyenDucTam2801@users.noreply.github.com>
2023-05-28 01:05:59 +0700

    fix gametate

--- src/GameState/MenuState.java
+++ src/GameState/MenuState.java
@@ -86,7 +86,7 @@ public class MenuState extends GameState {
     private void select() {
 
             if (currentChoice == 0) {
-                gsm.setState(2);
+                gsm.setState(1);
             }
             if (currentChoice == 1) {
                 //help
